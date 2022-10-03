acd00d7b2b5a4faec961a0261acdde6d79b371cc
zalobalay8 <92026671+zalobalay8@users.noreply.github.com>
2022-10-03 09:39:34 +0200

    DatePicker

--- FormularioRegistroGBL/app/src/main/java/com/liceolapaz/des/gbl/formularioregistrogbl/MainActivity.kt
+++ FormularioRegistroGBL/app/src/main/java/com/liceolapaz/des/gbl/formularioregistrogbl/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.liceolapaz.des.gbl.formularioregistrogbl
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity() : AppCompatActivity() {
     private lateinit var inputPais: Spinner
     private lateinit var name: EditText
     private lateinit var secondName: EditText
@@ -17,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var phone: EditText
     private lateinit var btnRegister: Button
     private lateinit var txtExit: TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,10 +47,11 @@ class MainActivity : AppCompatActivity() {
         password2 = findViewById(R.id.inputPassword2)
         password = findViewById(R.id.inputPassword)
         sexo = findViewById(R.id.grbGrupo1)
+        var genero=recuperarGenero(sexo)
         fecha = findViewById(R.id.inputFecha)
         btnRegister = findViewById(R.id.btnReg)
         txtExit = findViewById(R.id.txtExit)
-
+        var fechaPut = extraerFecha(fecha)
 
 
 
@@ -62,20 +68,52 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this@MainActivity, ShowInformation::class.java)
 
 
-            intent.putExtra("NAME", name.text.toString())
-            intent.putExtra("SNAME", secondName.text.toString())
-            intent.putExtra("PHONE", phone.text.toString())
-            intent.putExtra("MAIL", mail.text.toString())
-            intent.putExtra("SEX", sexo.checkedRadioButtonId.toString())
-            intent.putExtra("FECHA", fecha.text.toString())
+                intent.putExtra("NAME", name.text.toString())
+                intent.putExtra("SNAME", secondName.text.toString())
+                intent.putExtra("PHONE", phone.text.toString())
+                intent.putExtra("MAIL", mail.text.toString())
+                intent.putExtra("SEX", genero.toString())
+                intent.putExtra("FECHA", fechaPut.toString())
 
-            startActivity(intent)
+                startActivity(intent)
+            }
+
         }
 
     }
 
+    private fun recuperarGenero(sexo: RadioGroup?): Toast? {
+        val genero: RadioButton? = sexo?.let { findViewById(it.checkedRadioButtonId) }
+        if (genero != null) {
+            return Toast.makeText(applicationContext, genero.text, Toast.LENGTH_SHORT)
+        }
+            return null
+    }
+    private fun extraerFecha(fecha: EditText) : String {
+        var cal = Calendar.getInstance()
+        var year = cal.get(Calendar.YEAR)
+        var month = cal.get(Calendar.MONTH)
+        var dayOfMonth = cal.get(Calendar.DAY_OF_MONTH)
 
-}
+        fecha.setOnClickListener {
+            var monthr=month+1
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { view, year, month, dayOfMonth ->
+                    fecha.setText("$dayOfMonth/$monthr/$year")
+                },
+                year,
+                month,
+                dayOfMonth
+            )
 
+            datePickerDialog.show()
 
         }
+
+        return "$dayOfMonth/$month/$year"
+    }
+
+
+    }
+
